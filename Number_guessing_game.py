9af88228477b5eca7dda9e9a9ce075d362ac54f9
Zaynab Maleki <zizimrad@gmail.com>
2024-12-25 14:51:16 +0330

    random number

---
+++ Number_guessing_game.py
@@ -0,0 +1,12 @@
+import random
+
+print("Hello welcome to the game!")
+
+number_to_guess = random.randint(1,100)
+user_input =int(input("Guest a number between 1-100"))
+
+
+if answer ==x :
+    print("You won!")
+elif answer < x:
+    print("Lower")
